496d6efdf2fe44fc9d143e64bddbf47547d813b0
Siddharth VP <siddharthvp@gmail.com>
2020-05-01 20:29:51 +0530

    update petscan-resort.js

--- stub-resort/petscan-resort.js
+++ stub-resort/petscan-resort.js
@@ -1,11 +1,13 @@
+// TODO: write documentation
+
 const {utils, log, argv} = require('../botbase');
 const auth = require('../.auth');
 
 var StubTagManager = require('./StubTagManager');
 var getPetscanList = require('./getPetscanList');
 
-const mwn = require('../mwn/src/index');
-const bot = new mwn.bot({
+const mwn = require('../mwn/src/bot');
+const bot = new mwn({
 	apiUrl: 'https://en.wikipedia.org/w/api.php',
 	username: auth.bot_username,
 	password: auth.bot_password
@@ -25,18 +27,29 @@ bot.loginGetToken().then(function() {
 
 			if (Array.isArray(argv.remove)) {
 				argv.remove.forEach(tag => stm.removeTag(tag));
-			} else {
+			} else if (argv.remove) {
 				stm.removeTag(argv.remove);
 			}
 			if (Array.isArray(argv.add)) {
-				argv.add.forEach(tag => stm.removeTag(tag));
-			} else {
+				argv.add.forEach(tag => stm.addTag(tag));
+			} else if (argv.add) {
 				stm.addTag(argv.add);
 			}
-
+			
+			var summary;
+			if (stm.removedTags.length && stm.addedTags.length) {
+				summary = `Stub sorting: replacing ${utils.makeSentence(stm.removedTags)} with ${utils.makeSentence(stm.addedTags)}`;
+			} else if (stm.removedTags.length) {
+				summary = `Stub sorting: removing ${utils.makeSentence(stm.removedTags)}}`;
+			} else if (stm.addedTags.length) {
+				summary = `Stub sorting: adding ${utils.makeSentence(stm.addedTags)}`;
+			} else {  // should only happen if there are no tags added or removed - only re-formatting is taking place
+				// can't abort the edit as bot.edit doensn't let us do so 
+				summary = `Stub sorting`;	
+			}
 			return {
 				text: stm.getText(),
-				summary: `Stub sorting: replacing ${utils.makeSentence(stm.removedTags)} with ${utils.makeSentence(stm.addedTags)}`,
+				summary: summary,
 				minor: 1
 			};
 		}).catch(err => {
@@ -44,6 +57,6 @@ bot.loginGetToken().then(function() {
 			return Promise.reject(err);
 		});
 
-	}, 3000).then(console.log);
+	}, argv.sleep || 3000).then(console.log);
 
 });
