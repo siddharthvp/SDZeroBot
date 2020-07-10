263fb8a134918d6ea95c4caa7c54966d82dcd673
Siddharth VP <siddharthvp@gmail.com>
2020-07-10 18:40:19 +0530

    test bundling AfDs

--- run-job.py
+++ run-job.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+
 import sys
-import subprocess
+import os
 
 try:
 	runjob = sys.argv[1].lower()
@@ -7,13 +9,13 @@ except IndexError:
 	print('USAGE: python run-task.py <jobname>')
 	sys.exit(1)
 
-with open('~/SDZeroBot/crontab', 'r') as crontab:
+with open('/data/project/sdzerobot/SDZeroBot/crontab', 'r') as crontab:
 	lines = crontab.read().splitlines() 
 	lines = [line for line in lines if not line.startswith('#') and line.strip() is not '']
 	for line in lines:
 		job = line.split()[8].lower()
 		if job[4:] == runjob:
-			command = line.split()[5:]
-			print('> ' + ' '.join(command))
-			subprocess.run(command)
+			command = ' '.join(line.split()[5:])
+			print('> ' + command)
+			os.popen(command)
 			break
