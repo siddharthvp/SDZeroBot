f5e370d3aeb2d8a4122981303f414603682a6750
Siddharth VP <siddharthvp@gmail.com>
2020-08-23 22:47:01 +0530

    setup prod-watch

--- prod-watch/runner.js
+++
@@ -1,7 +0,0 @@
-const {log, argv} = require('../botbase');
-
-// Generate the report for a particular date 
-// - after 7 days, save the de-prod rationale and editor
-// - after 15 days
-
-var d = new Date()
