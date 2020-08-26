3c45163b3a6875c8e4257802eed8dd19f8e7ea79
Siddharth VP <siddharthvp@gmail.com>
2020-08-26 18:22:29 +0530

    g13: add query.js for debugging

---
+++ g13-watch/query.js
@@ -0,0 +1,17 @@
+const {log, argv} = require('../botbase');
+
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
+
+(async function() {
+	
+	const db = await sqlite.open({
+		filename: './g13.db',
+		driver: sqlite3.Database
+	});
+
+	log('[S] Connected to the g13 database.');
+
+	log(await db.all(argv[0]));
+
+})();
