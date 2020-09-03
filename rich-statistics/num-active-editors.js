503347c5a1515cd72b58a4078056480b2f367671
Siddharth VP <siddharthvp@gmail.com>
2020-09-03 22:12:36 +0530

    richstat

--- rich-statistics/num-active-editors.js
+++ rich-statistics/num-active-editors.js
@@ -34,16 +34,19 @@ table.addHeaders([
 	{label: 'Value', style: 'width: 20em'}
 ]);
 
-table.addRow([ 'Number of users active (1+ action) in the last 30 days', orz(userswithcount[1]) + '<ref>Users active in the last 30 days according to [[mw:API:Allusers]] with auactiveusers=1, which is different from figure on [[Special:Statistics]] ([[phab:T261290]]). Users with recentchanges value equal to 0 ([[phab:T261282]]) are not counted</ref>' ]);
+table.addRow([ 'Number of users active (1+ action) in the last 30 days', orz(userswithcount[1]) + '<ref>Users active in the last 30 days according to [[mw:API:Allusers]] with auactiveusers=1, which is different from the figure on [[Special:Statistics]] ([[phab:T261290]]). Users with recentchanges value equal to 0 ([[phab:T261282]]) are not counted</ref>' ]);
 table.addRow([ 'Users with 5+ actions in the last 30 days', orz(userswithcount[5]) ]);
 table.addRow([ 'Users with 10+ actions in the last 30 days', orz(userswithcount[10]) ]);
 table.addRow([ 'Users with 50+ actions in the last 30 days', orz(userswithcount[50]) ]);
 table.addRow([ 'Users with 100+ actions in the last 30 days', orz(userswithcount[100]) ]);
-table.addRow([ 'Users with 100+ actions in the last 30 days', orz(userswithcount[500]) ]);
-table.addRow([ 'Users with 100+ actions in the last 30 days', orz(userswithcount[1000]) ]);
+table.addRow([ 'Users with 500+ actions in the last 30 days', orz(userswithcount[500]) ]);
+table.addRow([ 'Users with 1000+ actions in the last 30 days', orz(userswithcount[1000]) ]);
+table.addRow([ 'Users with 5000+ actions in the last 30 days', orz(userswithcount[5000]) ]);
 
 let wikitext = 
-`${table.getText()}
+`{{hatnote|Last updated by [[User:SDZeroBot|SDZeroBot]] at ~~~~~}}
+
+${table.getText()}
 
 ==Notes==
 {{reflist}}
