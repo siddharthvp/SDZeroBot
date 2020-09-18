33f1e334514891e1e4e778ce8b9269a8d42f2cdb
Siddharth VP <siddharthvp@gmail.com>
2020-09-18 23:11:31 +0530

    afd-notifier: improvise

--- afd-notifcations/notifier.js
+++ afd-notifcations/notifier.js
@@ -1,4 +1,4 @@
-const {bot, log, xdate, emailOnError, utils, mwn} = require('../botbase');
+const {bot, log, emailOnError, utils, mwn, argv} = require('../botbase');
 
 process.chdir(__dirname);
 
@@ -32,7 +32,7 @@ class Notifier {
 			emailOnError(e, 'notifier');
 		} finally {
 			let wikitext = `~~~~~\n\n${notifier.table.getText()}`;
-			bot.save('User:SDZeroBot/AfD notifier/log', wikitext, 'Logging dry run');
+			bot.save('User:SDZeroBot/AfD notifier/log', wikitext, 'Logging');
 
 			utils.saveObject('notifications', notifier.notifications);
 			log(notifier.aborts);
@@ -56,7 +56,11 @@ class Notifier {
 			namePredicate: name => name === '/user'
 		});
 		for (let item of items) {
-			this.config[item.getValue(1)] = {
+			let username = item.getValue(1);
+			if (!username) {
+				continue;
+			}
+			this.config[username] = {
 				bytes: item.getValue('bytes') || ByteDefault,
 				percent: item.getValue('percent') || PercentDefault
 			};
@@ -65,7 +69,11 @@ class Notifier {
 		let exclusions = sections[4].content;
 		new bot.wikitext(exclusions).parseTemplates({
 			namePredicate: name => name === 'U'
-		}).map(t => t.getValue(1)).forEach(user => {
+		}).forEach(t => {
+			let user = t.getValue(1);
+			if (!user) {
+				return;
+			}
 			this.config[user] = {
 				percent: 101
 			};
@@ -79,8 +87,8 @@ class Notifier {
 	 * @returns {string[]}
 	 */
 	async getAfDs() {
-		let date = new xdate().subtract(1, 'day').format('YYYY MMMM D');
-		const afdlog = new bot.page(`Wikipedia:Articles for deletion/Log/${date}`);
+		this.date = new bot.date().subtract(1, 'day').format('YYYY MMMM D');
+		const afdlog = new bot.page(`Wikipedia:Articles for deletion/Log/${this.date}`);
 		const text = await afdlog.text();
 		let rgx = /\{\{(Wikipedia:Articles for deletion\/.*?)}}(?!<!--Relisted-->)/mg;
 		this.afds = {};
@@ -99,17 +107,24 @@ class Notifier {
 
 	async processAfD([afd, afdtext]) {
 		log(`[+] Processing ${afd}`);
-		// let tsmatch = afdtext.match(/\d{2}:\d{2}, \d{1,2} \w+ \d{4} \(UTC\)/);
-		// if (!tsmatch || !tsmatch[0]) {
-		// 	return log(`[E] Failed to read a timestamp in ${afd}`);
-		// }
-		// let ts = tsmatch[0];
-		// let date = new xdate(ts);
-		// if (date.setHours(0, 0, 0, 0) !== new xdate().subtract(1, 'day').setHours(0, 0, 0, 0)) {
-		// 	// not actually from yesterday; might be a manual relist
-		// 	log(`[W] ${afd} not from yesterday (manual relist)?`);
-		// 	return;
-		// }
+		let tsmatch = afdtext.match(/\d{2}:\d{2}, \d{1,2} \w+ \d{4} \(UTC\)/);
+		if (!tsmatch || !tsmatch[0]) {
+			return log(`[E] Failed to read a timestamp in ${afd}`);
+		}
+		let ts = tsmatch[0];
+		let date = new bot.date(ts);
+		if (date.format('YYYY MMMM D') !== this.date) {
+			// not actually from yesterday; might be a manual relist
+			log(`[W] ${afd} not from yesterday (manual relist)?`);
+			log(`[W] Date found: ${date.format('YYYY MMMM d')}, expected: ${this.date}`);
+			return;
+		}
+
+		// don't notify if discussion is already closed (likely a speedy keep)
+		if (/The following discussion is an archived debate/.test(afdtext)) {
+			return log(`[W] ${afd} already closed`);
+		}
+
 		let articleRgx = /\{\{la\|(.*?)\}\}/g;
 		let articles = [];
 		let match;
@@ -124,34 +139,48 @@ class Notifier {
 			'already-notified': 0,
 			'blocked': 0,
 			'blocked-indef': 0,
-			'locked': 0
-		}
+			'locked': 0,
+			'user-bot': 0
+		};
+
 		for (let article of articles) {
 			let authors = await this.getAuthorsForArticle(article);
-			log(`[i] Got authors: ${authors.filter(e => e.percent > PercentDefault && e.bytes > ByteDefault).map(e => e.name).join(', ')}`);
-			for (let {name, percent, bytes} of authors) {
-				if (percent > ((this.config[name] && this.config[name].percent) || PercentDefault) &&
-					bytes > ((this.config[name] && this.config[name].bytes) || ByteDefault)) {
+			log(`[i] Got authors: ${authors.filter(e => e.percent > PercentDefault && e.bytes > ByteDefault).map(e => e.name).join(', ')}`); // best-effort logging
 
-					let cmt = '';
-					await this.notifyUser(name, article, afd).then(() => {
-						log(`[T] Notified ${name} about ${article}`);
-						cmt = 'Notified';
-						this.notifications.push({name, article, afd});
-					}, (abortreason) => {
-						if (typeof this.aborts[abortreason] === 'number') {
-							this.aborts[abortreason]++;
-							cmt = abortreason;
-						} else {
-							log(`[E] Unknown rejection (${abortreason}): ${name}, ${afd}, ${article}`);
-						}
-					});
-					this.table.addRow([`[[${afd}]]`, `[[${article}]] ({{history|1=${article}|2=h}})`, `[[User:${name}|${name}]]`,
-						bytes,
-						`[https://xtools.wmflabs.org/authorship/en.wikipedia.org/${article.replace(/ /g, '_')} ${Math.round(percent*100)}%]`,
-						cmt
-					]);
+			for (let {name, percent, bytes, refBytes} of authors) {
+				if (percent < ((this.config[name] && this.config[name].percent) || PercentDefault)) {
+					continue;
 				}
+				if (bytes < ((this.config[name] && this.config[name].bytes) || ByteDefault)) {
+					continue;
+				}
+
+				// contribution was entirely references (could be refill, citation bot, etc)
+				if (refBytes / (refBytes + bytes) > 0.9) {
+					continue;
+				}
+
+				let cmt = '';
+				await this.notifyUser(name, article, afd).then(() => {
+					log(`[T] Notified ${name} about ${article}`);
+					cmt = 'Sent notification';
+					this.notifications.push({name, article, afd});
+				}, (abortreason) => {
+					if (this.aborts[abortreason] !== undefined) {
+						this.aborts[abortreason]++;
+						cmt = abortreason;
+					} else {
+						log(`[E] Unknown rejection (${abortreason}): ${name}, ${afd}, ${article}`);
+					}
+				});
+				this.table.addRow([
+					`[[${afd}]]`,
+					`[[${article}]] ({{history|1=${article}|2=h}})`,
+					`[[User:${name}|${name}]]`,
+					bytes,
+					`[https://xtools.wmflabs.org/authorship/en.wikipedia.org/${article.replace(/ /g, '_')} ${Math.round(percent*100)}%]`,
+					cmt
+				]);
 			}
 			await bot.sleep(500); // pause for a while after querying WikiWho
 		}
@@ -159,15 +188,16 @@ class Notifier {
 
 	async getAuthorsForArticle(article) {
 
-		const data = await this.queryAuthors(article);
-		return data.users.map(us => {
-			return {
-				name: us.name,
-				percent: us.percent,
-				bytes: us.bytes
-			};
-		});
-
+		let data;
+		try {
+			data = await this.queryAuthors(article);
+		} catch(err) {
+			if (/does not exist/.test(err.message)) {
+				log(`[W] ${article} does not exist`);
+				return [];
+			}
+		}
+		return data.users;
 	}
 
 	/**
@@ -186,27 +216,91 @@ class Notifier {
 		let json;
 		try {
 			json = await bot.rawRequest({
-				url: `https://api.wikiwho.net/${langcodematch[1]}/api/v1.0.0-beta/latest_rev_content/${encodeURIComponent(title)}/?o_rev_id=true&editor=true`
+				url: `https://api.wikiwho.net/${langcodematch[1]}/api/v1.0.0-beta/latest_rev_content/${encodeURIComponent(title)}/?editor=true`
 			});
 		} catch(err) {
-			if (/does not exist/.test(err.response.data.Error)) {
-				log(`[W] ${title} does not exist`);
-				return { users: [] }; // kludge: dummy object
-			}
+			throw new Error(err && err.response && err.response.data
+				&& err.response.data.Error);
 		}
 
 		const tokens = Object.values(json.revisions[0])[0].tokens;
 
 		let data = {
-				totalBytes: 0,
-				users: []
-			}, userdata = {};
+			totalBytes: 0,
+			users: []
+		}, userdata = {};
 
-		for (let token of tokens) {
+		function checkCoalesce(tokens, i, j, value) {
+			return tokens.slice(i, j).map(t => t.str).join('') === value;
+		}
+
+		let inRef = false, inCmt = false, inCategory = false;
+
+		for (let i = 0; i < tokens.length; i++) {
+
+			if (inCmt) {
+				if (tokens[i].str === '-->') {
+					inCmt = false;
+				}
+				continue;
+			} else {
+				if (tokens[i].str === '<!--') {
+					inCmt = true;
+					continue;
+				}
+			}
+
+			if (inCategory) {
+				if (tokens[i].str === ']]') {
+					inCategory = false;
+				}
+				continue;
+			} else {
+				if (checkCoalesce(tokens, i, i + 3, '[[category:')) {
+					inCategory = true;
+					i += 2;
+					continue;
+				}
+			}
+
+			// Consider references as content, but avoid notifying users
+			// whose only contribs are the references (this handles use of refill,
+			// citation bot, etc)
+			let addRefBytes = function(...tokens) {
+				for (let token of tokens) {
+					let editor = token.editor;
+					if (!userdata[editor]) {
+						userdata[editor] = { bytes: 0, refBytes: 0 };
+					}
+					userdata[editor].refBytes += token.str.length;
+					if (editor.startsWith('0|')) { // IP
+						userdata[editor].name = editor.slice(2);
+					}
+				}
+			};
+
+			if (inRef) {
+				addRefBytes(tokens[i]);
+				if (checkCoalesce(tokens, i, i + 4, '</ref>')) {
+					addRefBytes(tokens[i+1], tokens[i+2], tokens[i+3]);
+					i += 3;
+					inRef = false;
+				}
+				continue;
+			} else {
+				if (checkCoalesce(tokens, i, i + 2, '<ref')) {
+					inRef = true;
+					addRefBytes(tokens[i], tokens[i+1]);
+					i++;
+					continue;
+				}
+			}
+
+			let token = tokens[i];
 			data.totalBytes += token.str.length;
-			let editor = token['editor'];
+			let editor = token.editor;
 			if (!userdata[editor]) {
-				userdata[editor] = { bytes: 0 };
+				userdata[editor] = { bytes: 0, refBytes: 0 };
 			}
 			userdata[editor].bytes += token.str.length;
 			if (editor.startsWith('0|')) { // IP
@@ -231,17 +325,17 @@ class Notifier {
 			});
 		});
 
-		data.users = Object.entries(userdata).map(([userid, {bytes, name, percent}]) => {
+		data.users = Object.entries(userdata).map(([userid, {bytes, refBytes, name, percent}]) => {
 			return {
 				id: userid,
-				name: name,
-				bytes: bytes,
-				percent: percent
+				name,
+				bytes,
+				percent,
+				refBytes
 			};
 		}).sort((a, b) => {
-			a.bytes < b.bytes ? 1 : -1;
+			return a.bytes < b.bytes ? 1 : -1;
 		});
-
 		return data;
 	}
 
@@ -249,6 +343,10 @@ class Notifier {
 	// XXX: everything here can be done for multiple users at once, rewrite this
 	async notifyUser(username, article, afd) {
 		let user = new bot.user(username);
+		if (/bot\b/i.test(username)) {
+			log(`[W] Didn't notify ${username} as user is bot?`);
+			return Promise.reject('bot-user');
+		}
 		try {
 			let text = await user.talkpage.text();
 			if (/\{\{nobots\}\}/i.test(text)) { // TODO: also check for deny=SDZeroBot
@@ -275,7 +373,7 @@ class Notifier {
 					log(`[C] Not notifying ${username} as account is indef-blocked`);
 					return Promise.reject('blocked-indef');
 				}
-				if (new xdate().add(7, 'days').isBefore(new xdate(blockinfo.blockexpiry))) {
+				if (new bot.date().add(7, 'days').isBefore(new bot.date(blockinfo.blockexpiry))) {
 					log(`[C] Not notifying ${username} as account is blocked for 7+ days`);
 					return Promise.reject('blocked');
 				}
@@ -288,24 +386,155 @@ class Notifier {
 			}
 		}
 
-		// return Promise.resolve();
-		log(`[+] Notifying ${username}`);
+		if (!argv.dry) {
+			log(`[+] Notifying ${username}`);
 
-		// bot.newSection() doesn't really check out
-		await bot.request({
-			action: 'edit',
-			title: 'User talk:' + username,
-			bot: 1,
-			summary: `Nomination of [[${article}]] for deletion at [[${afd}|AfD]]`,
-			appendtext: `\n\n{{subst:User:SDZeroBot/AfD notifier/template|1=${article}|afdpage=${afd}}}`,
-			token: bot.csrfToken
-		}).then(() => {
-			// log(`[S] Notified ${username}`);
-		}, err => { // errors are logged, don't cause task to stop
-			log(`[E] ${username} couldn't be notified due to error: ${err.code}: ${err.info}`);
-		});
-		await bot.sleep(5000);
+			// bot.newSection() doesn't really check out
+			await bot.request({
+				action: 'edit',
+				title: 'User talk:' + username,
+				bot: 1,
+				summary: `Nomination of [[${article}]] for deletion at [[${afd}|AfD]]`,
+				appendtext: `\n\n{{subst:User:SDZeroBot/AfD notifier/template|1=${article}|afdpage=${afd}}}`,
+				token: bot.csrfToken
+			}).then(() => {
+				// log(`[S] Notified ${username}`);
+			}, err => { // errors are logged, don't cause task to stop
+				log(`[E] ${username} couldn't be notified due to error: ${err.code}: ${err.info}`);
+			});
+			await bot.sleep(5000);
+		}
+
 	}
 }
 
 Notifier.init().catch(err => emailOnError(err, 'afd-notifier'));
+
+// // BROWSER TESTING:
+
+// Object.values((await $.get(`https://api.wikiwho.net/en/api/v1.0.0-beta/latest_rev_content/${encodeURIComponent(Morebits.pageNameNorm)}/?editor=true`)).revisions[0])[0].tokens
+
+// await (async function queryAuthors(title) {
+// 		let json;
+// 		try {
+// 			json = await $.get(`https://api.wikiwho.net/en/api/v1.0.0-beta/latest_rev_content/${encodeURIComponent(Morebits.pageNameNorm)}/?editor=true`);
+// 		} catch(err) {
+// 			throw new Error(err && err.response && err.response.data
+// 				&& err.response.data.Error);
+// 		}
+
+// 		const tokens = Object.values(json.revisions[0])[0].tokens;
+
+// 		let data = {
+// 				totalBytes: 0,
+// 				users: []
+// 			}, userdata = {};
+
+// 		function checkCoalesce(tokens, i, j, value) {
+// 			return tokens.slice(i, j).map(t => t.str).join('') === value;
+// 		}
+
+// 		let inRef = false, inCmt = false, inCategory = false;
+
+// 		for (let i = 0; i < tokens.length; i++) {
+
+// 			if (inCmt) {
+// 				if (tokens[i].str === '-->') {
+// 					inCmt = false;
+// 				}
+// 				continue;
+// 			} else {
+// 				if (tokens[i].str === '<!--') {
+// 					inCmt = true;
+// 					continue;
+// 				}
+// 			}
+
+// 			if (inCategory) {
+// 				if (tokens[i].str === ']]') {
+// 					inCategory = false;
+// 				}
+// 				continue;
+// 			} else {
+// 				if (checkCoalesce(tokens, i, i + 3, '[[category:')) {
+// 					inCategory = true;
+// 					i += 2;
+// 					continue;
+// 				}
+// 			}
+
+// 			// Consider references as content, but avoid notifying users
+// 			// whose only contribs are the references (this handles use of refill,
+// 			// citation bot, etc)
+// 			let addRefBytes = function(...tokens) {
+// 				for (let token of tokens) {
+// 					let editor = token.editor;
+// 					if (!userdata[editor]) {
+// 						userdata[editor] = { bytes: 0, refBytes: 0 };
+// 					}
+// 					userdata[editor].refBytes += token.str.length;
+// 					if (editor.startsWith('0|')) { // IP
+// 						userdata[editor].name = editor.slice(2);
+// 					}
+// 				}
+// 			};
+
+// 			if (inRef) {
+// 				addRefBytes(tokens[i]);
+// 				if (checkCoalesce(tokens, i, i + 4, '</ref>')) {
+// 					addRefBytes(tokens[i+1], tokens[i+2], tokens[i+3]);
+// 					i += 3;
+// 					inRef = false;
+// 				}
+// 				continue;
+// 			} else {
+// 				if (checkCoalesce(tokens, i, i + 2, '<ref')) {
+// 					inRef = true;
+// 					addRefBytes(tokens[i], tokens[i+1]);
+// 					i++;
+// 					continue;
+// 				}
+// 			}
+
+// 			let token = tokens[i];
+// 			data.totalBytes += token.str.length;
+// 			let editor = token.editor;
+// 			if (!userdata[editor]) {
+// 				userdata[editor] = { bytes: 0, refBytes: 0 };
+// 			}
+// 			userdata[editor].bytes += token.str.length;
+// 			if (editor.startsWith('0|')) { // IP
+// 				userdata[editor].name = editor.slice(2);
+// 			}
+// 		}
+
+// 		Object.entries(userdata).map(([userid, {bytes}]) => {
+// 			userdata[userid].percent = bytes / data.totalBytes;
+// 			if (userdata[userid].percent < 0.02) {
+// 				delete userdata[userid];
+// 			}
+// 		});
+
+// 		await new mw.Api().get({
+// 			"action": "query",
+// 			"list": "users",
+// 			"ususerids": Object.keys(userdata).filter(us => !us.startsWith('0|')) // don't lookup IPs
+// 		}).then(json => {
+// 			json.query.users.forEach(us => {
+// 				userdata[String(us.userid)].name = us.name;
+// 			});
+// 		});
+
+// 		data.users = Object.entries(userdata).map(([userid, {bytes, refBytes, name, percent}]) => {
+// 			return {
+// 				id: userid,
+// 				name,
+// 				bytes,
+// 				percent,
+// 				refBytes
+// 			};
+// 		}).sort((a, b) => {
+// 			return a.bytes < b.bytes ? 1 : -1;
+// 		});
+// 		return data;
+// 	})().then(res => res.users)
