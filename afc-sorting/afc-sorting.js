10783bd63ad3318305d39e3efba0fb44e882df9c
Siddharth VP <siddharthvp@gmail.com>
2020-09-27 17:51:51 +0530

    afc: data type fix needed for node 14

--- afc-sorting/afc-sorting.js
+++ afc-sorting/afc-sorting.js
@@ -246,7 +246,7 @@ process.chdir(__dirname);
 		}
 		var pagetext = `{{Wikipedia:AfC sorting/header|count=${count} (${diff})|date=${accessdate}|ts=~~~~~}}<includeonly><section begin=lastupdate />${new bot.date().format('D MMMM YYYY')}<section end=lastupdate /></includeonly>\n`;
 
-		fs.writeFileSync('./previousRunCount.txt', count);
+		fs.writeFileSync('./previousRunCount.txt', String(count));
 
 		Object.keys(sorter).sort(OresUtils.sortTopics).forEach(function(topic) {
 
