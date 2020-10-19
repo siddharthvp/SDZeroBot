9358af22abd257df90a7460430078e68962f32f1
Siddharth VP <siddharthvp@gmail.com>
2020-10-19 20:08:58 +0530

    afd-notifier: fix type in file name *_*

--- stub-resort/petscan-resort.js
+++ stub-resort/petscan-resort.js
@@ -1,27 +1,19 @@
 /**
  * node petscan-resort
- * 
+ *
  * command line arguments:
- * 
+ *
  * --link 	: petscan link from which to get articles
  * --add  	: add a stub tag, this arg can be given multiple times
  * --remove : remove a stub tag if it exists, this arg can be given multiple times
  * --sleep 	: (default 3000) number of milliseconds to pause after each edit
  */
 
-const {utils, mwn, log, argv} = require('../botbase');
-const auth = require('../.auth');
-
+const {utils, bot, log, argv} = require('../botbase');
 var StubTagManager = require('./StubTagManager');
 var getPetscanList = require('./getPetscanList');
 
-const bot = new mwn({
-	apiUrl: 'https://en.wikipedia.org/w/api.php',
-	username: auth.bot_username,
-	password: auth.bot_password
-});
-
-bot.loginGetToken().then(function() {
+bot.getTokensAndSiteInfo().then(function() {
 	return getPetscanList(argv.link);
 
 }).then(pagelist => {
@@ -36,8 +28,8 @@ bot.loginGetToken().then(function() {
 			var tagsToAdd = Array.isArray(argv.add) ? argv.add : [ argv.add ];
 			var tagsToRemove = Array.isArray(argv.remove) ? argv.remove : [ argv.remove ];
 			tagsToAdd.forEach(tag => stm.addTag(tag));
-			tagsToRemove.forEach(tag => stm.removeTag(tag)); 
-			
+			tagsToRemove.forEach(tag => stm.removeTag(tag));
+
 			var summary;
 			if (stm.removedTags.length && stm.addedTags.length) {
 				summary = `Stub sorting: replacing ${utils.makeSentence(stm.removedTags)} with ${utils.makeSentence(stm.addedTags)}`;
@@ -45,8 +37,8 @@ bot.loginGetToken().then(function() {
 				summary = `Stub sorting: removing ${utils.makeSentence(stm.removedTags)}}`;
 			} else if (stm.addedTags.length) {
 				summary = `Stub sorting: adding ${utils.makeSentence(stm.addedTags)}`;
-			} else {  
-				return; // no edit	
+			} else {
+				return; // no edit
 			}
 			return {
 				text: stm.getText(),
