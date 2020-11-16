496ea707488618d8015a6d421f54b488d8d4fa9c
Siddharth VP <siddharthvp@gmail.com>
2020-11-16 23:20:04 +0530

    g13-watch: try again

--- g13-watch/make-report-new.js
+++ g13-watch/make-report-new.js
@@ -1,83 +1,75 @@
 // npm run make
 
 const {bot, log, toolsdb, emailOnError, mwn} = require('../botbase');
+const {saveWithBlacklistHandling} = require('../tasks/commons');
 
 (async function() {
 
-await bot.getTokensAndSiteInfo();
+	await bot.getTokensAndSiteInfo();
 
-const db = await new toolsdb('g13watch_p').connect();
-log('[S] Connected to the g13 database.');
+	const db = await new toolsdb('g13watch_p').connect();
+	log('[S] Connected to the g13 database.');
 
-let table = new mwn.table();
-table.addHeaders([
-	{label: 'Date', style: 'width: 5em'},
-	{label: 'Draft', style: 'width: 18em'},
-	{label: 'Size', style: 'width: 4em'},
-	{label: 'Excerpt'}
-]);
-
-let end = new bot.date().setUTCHours(0,0,0,0);
-let start = new bot.date().subtract(24, 'hours').setUTCHours(0,0,0,0);
-
-let count = 0;
-
-const result = await db.query(`
-	SELECT * FROM g13
-	WHERE ts > ?
-	AND ts < ?
-`, [start.getTime() / 1000, end.getTime() / 1000]);
-
-result.forEach(row => {
-	count += 1;
-
-	let page = `[[${row.name}]]`;
-	if (row.desc) {
-		page += ` <small>${row.desc}</small>`
-	}
-
-	table.addRow([
-		new bot.date(row.ts * 1000).format('YYYY-MM-DD HH:mm'),
-		page,
-		row.size,
-		row.excerpt || ''
+	let table = new mwn.table();
+	table.addHeaders([
+		{label: 'Date', style: 'width: 5em'},
+		{label: 'Draft', style: 'width: 18em'},
+		{label: 'Size', style: 'width: 4em'},
+		{label: 'Excerpt'}
 	]);
-});
 
-let wikitable = table.getText();
-let yesterday = new bot.date().subtract(1, 'day');
+	let end = new bot.date().setUTCHours(0,0,0,0);
+	let start = new bot.date().subtract(24, 'hours').setUTCHours(0,0,0,0);
 
-let page = new bot.page('User:SDZeroBot/G13 Watch/new');
+	const result = await db.query(`
+		SELECT * FROM g13
+		WHERE ts > ?
+		AND ts < ?
+	`, [
+		start.getTime() / 1000,
+		end.getTime() / 1000
+	]);
 
-// let oldlinks = (await page.history('timestamp|ids', 3)).map(rev => {
-// 	let date = new bot.date(rev.timestamp).subtract(24, 'hours');
-// 	return `[[Special:Permalink/${rev.revid}|${date.format('D MMMM')}]]`;
-// }).join(' - ') + ' - {{history|2=older}}';
-
-let text = `{{/header|count=${count}|date=${yesterday.format('D MMMM YYYY')}|ts=~~~~~|oldlinks=}}<includeonly><section begin=lastupdate />${new bot.date().toISOString()}<section end=lastupdate /></includeonly>` 
-	+ `\n\n${wikitable}`;
-
-await page.save(text, 'Updating G13 report').catch(async err => {
-	if (err.code === 'spamblacklist') {
-		for (let site of err.response.error.spamblacklist.matches) {
-			text = text.replace(
-				new RegExp('https?:\\/\\/' + site, 'g'),
-				site
-			);
+	result.forEach(row => {
+		let page = `[[${row.name}]]`;
+		if (row.desc) {
+			page += ` <small>${row.desc}</small>`
 		}
-		await page.save(text, 'Updating G13 report');
-	} else {
-		return Promise.reject(err);
-	} 
-});
 
-// Delete data more than 3 days old:
-let ts_3days_old = Math.round(new bot.date().subtract(72, 'hours').getTime() / 1000);
-await db.run(`DELETE FROM g13 WHERE ts < ?`, [ts_3days_old]);
-db.end();
+		table.addRow([
+			new bot.date(row.ts * 1000).format('YYYY-MM-DD HH:mm'),
+			page,
+			row.size,
+			row.excerpt || ''
+		]);
+	});
 
-log(`[S] Deleted data more than 3 days old`);
+	let wikitable = table.getText();
+	let yesterday = new bot.date().subtract(1, 'day');
 
-log(`[i] Finished`);
+	let page = new bot.page('User:SDZeroBot/G13 Watch');
+
+	let oldlinks = '';
+	try {
+		oldlinks = (await page.history('timestamp|ids', 3)).map(rev => {
+			let date = new bot.date(rev.timestamp).subtract(24, 'hours');
+			return `[[Special:Permalink/${rev.revid}|${date.format('D MMMM')}]]`;
+		}).join(' - ') + ' - {{history|2=older}}';
+	} catch (e) {}
+
+	let text = `{{/header|count=${result.length}|date=${yesterday.format('D MMMM YYYY')}|ts=~~~~~|oldlinks=${oldlinks}}<includeonly><section begin=lastupdate />${new bot.date().toISOString()}<section end=lastupdate /></includeonly>`
+		+ `\n\n${wikitable}`;
+
+	await saveWithBlacklistHandling(page, text);
+
+	// Delete data more than 3 days old:
+	await db.run(`DELETE FROM g13 WHERE ts < ?`, [
+		Math.round(new bot.date().subtract(72, 'hours').getTime() / 1000)
+	]);
+	db.end();
+
+	log(`[S] Deleted data more than 3 days old`);
+
+	log(`[i] Finished`);
 
 })().catch(err => emailOnError(err, 'g13-watch-save'));
---
+++ g13-watch/save-to-db-new.ts
@@ -0,0 +1,95 @@
+// start job using: npm run start
+
+import {fs, bot, log, toolsdb, emailOnError, mysql} from '../botbase';
+
+const {preprocessDraftForExtract} = require('../tasks/commons');
+const TextExtractor = require('../TextExtractor')(bot);
+const auth = require('../.auth');
+
+async function main() {
+
+	await bot.getSiteInfo();
+
+	// Create a pool, but almost all the time only one connection will be used
+	// Each pool connection is released immediately after use
+	const pool = mysql.createPool({
+		host: 'tools.db.svc.eqiad.wmflabs',
+		user: auth.db_user,
+		password: auth.db_password,
+		port: 3306,
+		database: 's54328__g13watch_p',
+		waitForConnections: true,
+		connectionLimit: 5,
+		queueLimit: 10
+	});
+
+	await pool.execute(`
+		CREATE TABLE IF NOT EXISTS g13(
+			name varchar(255) unique,
+			description varchar(255),
+			excerpt blob,
+			size int,
+			ts int not null
+		) COLLATE 'utf8_unicode_ci'
+	`); // use utf8_unicode_ci so that MariaDb allows a varchar(255) field to have unique constraint
+	// max index column size is 767 bytes. 255*3 = 765 bytes with utf8, 255*4 = 1020 bytes with utf8mb4
+
+	const firstrow = await pool.query(`SELECT ts FROM g13 ORDER BY ts DESC LIMIT 1`)[0];
+
+	let stream = new bot.stream('recentchange', {
+		since: firstrow ?
+			new bot.date(firstrow.ts * 1000):
+			new bot.date().subtract(20, 'minutes')
+	});
+
+	stream.addListener({
+		wiki: 'enwiki',
+		type: 'categorize',
+		title: 'Category:Candidates for speedy deletion as abandoned drafts or AfC submissions'
+	}, async (data) => {
+		let match = /^\[\[:(.*?)\]\] added/.exec(data.comment);
+		if (!match) {
+			return;
+		}
+
+		let title = match[1];
+		let ts = data.timestamp;
+		log(`[+] Page ${title} at ${new bot.date(ts * 1000).format('YYYY-MM-DD HH:mm:ss')}`);
+		let pagedata = await bot.read(title, {
+			prop: 'revisions|description',
+			rvprop: 'content|size'
+		});
+		let text = pagedata?.revisions[0]?.content;
+		let size = pagedata.revisions[0].size;
+		let desc = pagedata.description;
+		if (desc && desc.size > 255) {
+			desc = desc.slice(0, 250) + ' ...';
+		}
+		let extract = TextExtractor.getExtract(text, 300, 550, preprocessDraftForExtract);
+
+		fs.appendFile(
+			__dirname + '/db-new.txt',
+			JSON.stringify([title, desc, extract, size, ts]) + '\n',
+			console.log
+		);
+
+		let conn;
+		try {
+			conn = await pool.getConnection();
+			await conn.execute(`INSERT INTO g13 VALUES(?, ?, ?, ?, ?)`, [title, desc, extract, size, ts]);
+		} catch (err) {
+			if (err.code === 'ER_DUP_ENTRY') {
+				log(`[W] ${title} entered category more than once`);
+				return;
+			}
+			emailOnError(err, 'g13-watch-db');
+		} finally {
+			await conn.release();
+		}
+	});
+}
+
+while (true) {
+	main().catch(err => emailOnError(err, 'g13-watch-db'));
+}
+
