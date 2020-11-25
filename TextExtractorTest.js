5a0be3af452098fd343617835fef70364192fbd7
Siddharth VP <siddharthvp@gmail.com>
2020-11-25 23:28:31 +0530

    TextExtractor: add test

--- TextExtractorTest.js
+++ TextExtractorTest.js
@@ -10,7 +10,7 @@ before(function() {
 
 it('removes templates on new lines', function() {
 
-	var text = `{{use dmy dates}}
+	let text = `{{use dmy dates}}
 {{infobox person 
 | name = Arthur A. Kempod
 | born = {{birth date and age|1988|04|09}}
@@ -22,6 +22,27 @@ it('removes templates on new lines', function() {
 Arthur was an fine tailor.
 `;
 
-	assert.equal(TE.removeTemplatesOnNewlines(text), '\n\nArthur was an fine tailor.\n'); 
+	assert.strictEqual(TE.removeTemplatesOnNewlines(text), '\n\nArthur was an fine tailor.\n');
 
+});
+
+it('runs preprocessHook', function () {
+	let text = `[[User:Example]] 21:09, 30 May 2020 (UTC){{AFC submission|t||ts=20200530210953|u=Harshit567|ns=118|demo=}}
+	
+==References==`;
+
+	let extract = TE.getExtract(text, 250, 500, function(text) {
+		let wkt = new bot.wikitext(text);
+		wkt.parseTemplates({
+			namePredicate: name => {
+				return /infobox/i.test(name) || name === 'AFC submission';
+			}
+		});
+		for (let template of wkt.templates) {
+			wkt.removeEntity(template);
+		}
+		return wkt.getText();
+	});
+
+	assert.strictEqual(extract, `[[User:Example]] 21:09, 30 May 2020 (UTC)`);
 });
