17c6216d3e23c51ed73692ffe1196cfa121f3abb
Siddharth VP <siddharthvp@gmail.com>
2021-05-06 19:43:12 +0530

    commit filesystem.ts: already in use by db-tabulator

---
+++ filesystem.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs';
+
+export function readFile(file) {
+	try {
+		return fs.readFileSync(file).toString();
+	} catch (e) {
+		return null;
+	}
+}
+
+export function writeFile(file, text) {
+	return fs.writeFileSync(file, text);
+}
