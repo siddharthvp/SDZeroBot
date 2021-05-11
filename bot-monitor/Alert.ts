e98b6f334e9f2f7492d44a328d454d52fb52396e
Siddharth VP <siddharthvp@gmail.com>
2021-05-11 18:20:21 +0530

    bot-monitor: enable talkpage alerts; remove other modes for now

--- bot-monitor/Alert.ts
+++ bot-monitor/Alert.ts
@@ -1,43 +1,45 @@
 import {argv, bot, log} from "../botbase";
 import {Rule, RuleError, Monitor} from "./index";
-
-// TODO: avoid redundant notifications
+import {makeTemplate} from "../utils";
 
 export class Alert {
     rule: Rule
     name: string
     actions: number
 
-    static pingpage = 'Wikipedia:Bot activity monitor/Pings'
-
     constructor(monitor: Monitor) {
         Object.assign(this, monitor);
     }
 
     async alert() {
-        // Alerts completely disabled for now:
-        return;
-
-        if (argv.dry || !this.rule.alertMode) {
+        if (argv.dry || !this.rule.alertPage) {
             return;
         }
-
-        if (this.rule.alertMode === 'talkpage') {
-            await this.alertTalkPage();
-        } else if (this.rule.alertMode === 'email') {
-            await this.alertEmail();
-        } else if (this.rule.alertMode === 'ping') {
-            await this.alertPing();
-        } else {
-            throw new RuleError(`Invalid alert mode: ${this.rule.alertMode}: must be "talkpage", "email" or "ping"`);
-        }
+        await this.alertTalkPage();
+        // if (this.rule.alertMode === 'talkpage') {
+        //     await this.alertTalkPage();
+        // } else if (this.rule.alertMode === 'email') {
+        //     await this.alertEmail();
+        // } else if (this.rule.alertMode === 'ping') {
+        //     await this.alertPing();
+        // } else {
+        //     throw new RuleError(`Invalid alert mode: ${this.rule.alertMode}: must be "talkpage", "email" or "ping"`);
+        // }
     }
 
     async alertTalkPage() {
-        await new bot.page(this.rule.alertPage).newSection(
-            this.getHeader(),
-            this.getMessage() + ' ~~~~',
-            {redirect: true, nocreate: true}
+        let page = new bot.page(this.rule.alertPage);
+        let text = await page.text();
+        let header = `== ${this.rule.bot}: ${this.rule.task} failure ==`;
+        if (text.includes(header)) {
+            log(`[i] Aborting notification for ${this.rule.bot} because it already exists`);
+            return;
+        }
+        log(`[i] Notifying for ${this.rule.bot}`);
+        await page.newSection(
+            header,
+            this.getMessage() + ' – ~~~~',
+            { redirect: true, nocreate: true }
         ).catch(err => {
             if (err.code === 'missingtitle') {
                 throw new RuleError(`Missing alert page: ${this.rule.alertPage}`);
@@ -47,42 +49,48 @@ export class Alert {
         });
     }
 
-    async alertEmail() {
-        await new bot.user(this.rule.emailUser).email(
-            this.getHeader(),
-            this.getMessage(),
-            {ccme: true}
-        ).catch(err => {
-            if (err.code === 'notarget') {
-                throw new RuleError(`Invalid username for email: ${this.rule.emailUser}`);
-            } else if (err.code === 'nowikiemail') {
-                throw new RuleError(`Email is disabled for ${this.rule.emailUser}`);
-            } else throw err;
-        });
-    }
-
-    async alertPing() {
-        let pingUser = this.rule.pingUser || await getBotOperator(this.rule.bot) || this.rule.bot;
-        await new bot.page(Alert.pingpage).edit((rev) => {
-            return {
-                appendtext: `{{re|${pingUser}}} ${this.rule.bot}'s task ${this.rule.task} failed: found ${this.actions} ${this.rule.action === 'edit' ? 'edits' : 'log actions'} against ${this.rule.minEdits} expected.`,
-                summary: `Reporting [[:User:${this.rule.bot}|${this.rule.bot}]]: ${this.rule.task}`
-            }
-        });
-    }
-
-    getHeader() {
-        if (typeof this.rule.header === 'string') {
-            return this.rule.header
-                .replace('$TASK', this.rule.task.replace(/\$/g, '$$$$'))
-                .replace('$BOT', this.rule.bot.replace(/\$/g, '$$$$'));
-        }
-        return `${this.rule.task} failure`; // default
-    }
-
     getMessage() {
-        return `The bot task ${this.name} failed to run per the requirements specified at [[${Monitor.configpage}]]. Found only ${this.actions} ${this.rule.action === 'edit' ? 'edits' : 'log actions'}, expected ${this.rule.minEdits}.`;
+        return makeTemplate('subst:Wikipedia:Bot activity monitor/Notification', {
+            bot: this.rule.bot,
+            task: this.rule.task,
+            action: this.rule.action === 'edit' ? 'edit' : `"${this.rule.action}" action`,
+            actual: String(this.actions),
+            expected: String(this.rule.minEdits),
+            duration: this.rule.duration
+        });
     }
+
+    // async alertEmail() {
+    //     await new bot.user(this.rule.emailUser).email(
+    //         this.getHeader(),
+    //         this.getMessage(),
+    //         {ccme: true}
+    //     ).catch(err => {
+    //         if (err.code === 'notarget') {
+    //             throw new RuleError(`Invalid username for email: ${this.rule.emailUser}`);
+    //         } else if (err.code === 'nowikiemail') {
+    //             throw new RuleError(`Email is disabled for ${this.rule.emailUser}`);
+    //         } else throw err;
+    //     });
+    // }
+    // static pingpage = 'Wikipedia:Bot activity monitor/Pings'
+    // async alertPing() {
+    //     let pingUser = this.rule.pingUser || await getBotOperator(this.rule.bot) || this.rule.bot;
+    //     await new bot.page(Alert.pingpage).edit((rev) => {
+    //         return {
+    //             appendtext: `{{re|${pingUser}}} ${this.rule.bot}'s task ${this.rule.task} failed: found ${this.actions} ${this.rule.action === 'edit' ? 'edits' : 'log actions'} against ${this.rule.minEdits} expected.`,
+    //             summary: `Reporting [[:User:${this.rule.bot}|${this.rule.bot}]]: ${this.rule.task}`
+    //         }
+    //     });
+    // }
+    // getHeader() {
+    //     if (typeof this.rule.header === 'string') {
+    //         return this.rule.header
+    //             .replace('$TASK', this.rule.task.replace(/\$/g, '$$$$'))
+    //             .replace('$BOT', this.rule.bot.replace(/\$/g, '$$$$'));
+    //     }
+    //     return `${this.rule.bot}: ${this.rule.task} failure`; // default
+    // }
 
 }
 
--- bot-monitor/Rule.ts
+++ bot-monitor/Rule.ts
@@ -1,23 +1,8 @@
-import {argv, bot, fs, mwn, path} from "../botbase";
-import {MwnDate} from "../../mwn";
+import {argv, bot, mwn, path} from "../botbase";
+import {MwnTitle, MwnDate} from "../../mwn";
 import {getFromDate} from "./index";
-
-export type BotConfigParam =
-    | 'bot'
-    | 'task'
-    | 'action'
-    | 'namespace'
-    | 'title'
-    | 'title_regex'
-    | 'summary'
-    | 'summary_regex'
-    | 'min_edits'
-    | 'duration'
-    | 'alert_mode'
-    | 'alert_page'
-    | 'email_user'
-    | 'header'
-    | 'ping_user'
+import {readFile} from "../utils";
+import {NS_USER_TALK} from "../namespaces";
 
 export type RawRule = Record<BotConfigParam, string>
 
@@ -31,23 +16,39 @@ export interface Rule {
     minEdits: number
     duration: string
     fromDate: MwnDate
-    alertMode: 'email' | 'talkpage' | 'ping'
-    alertPage: string
-    emailUser: string
-    header: string
-    pingUser: string
+    alertPage: MwnTitle
+
+    // alertMode: 'email' | 'talkpage' | 'ping'
+    // emailUser: string
+    // header: string
+    // pingUser: string
 }
 
-export class RuleError extends Error {
-    constructor(msg) {
-        super(msg);
-    }
-}
+export type BotConfigParam =
+    | 'bot'
+    | 'task'
+    | 'action'
+    | 'namespace'
+    | 'title'
+    | 'title_regex'
+    | 'summary'
+    | 'summary_regex'
+    | 'min_edits'
+    | 'duration'
+    | 'notify'
+
+    // | 'alert_mode'
+    // | 'alert_page'
+    // | 'email_user'
+    // | 'header'
+    // | 'ping_user'
+
+export class RuleError extends Error {}
 
 export async function fetchRules(): Promise<RawRule[]> {
     let text = !argv.fake ?
         await new bot.page('Wikipedia:Bot activity monitor/Configurations').text() :
-        fs.readFileSync(path.join(__dirname, 'fake-config.wikitext')).toString();
+        readFile(path.join(__dirname, 'fake-config.wikitext'));
 
     let templates = new bot.wikitext(text).parseTemplates({
         namePredicate: name => name === '/task'
@@ -69,21 +70,20 @@ export function parseRule(rule: RawRule): Rule {
     if (!rule.bot) {
         throw new RuleError(`No bot account specified!`);
     }
-    if (rule.alert_page) {
-        let title = bot.title.newFromText(rule.alert_page);
-        if (!title) {
-            throw new RuleError(`Invalid alert page: ${rule.alert_page}`);
-        } else if (title.namespace === 0) {
-            throw new RuleError(`Invalid alert page: ${rule.alert_page}`);
-        }
+    if (!rule.task) {
+        throw new RuleError(`No task name specified!`);
+    }
+    let alertPage = rule.notify && bot.title.newFromText(rule.notify, NS_USER_TALK);
+    if (rule.notify && (!alertPage || alertPage.namespace === 0)) {
+        throw new RuleError(`Invalid alert page: ${rule.notify}`);
     }
     if (rule.min_edits && isNaN(parseInt(rule.min_edits))) {
-        throw new RuleError(`Invalid min_edits: ${rule.min_edits}: must be a numbeer`);
+        throw new RuleError(`Invalid min_edits: ${rule.min_edits}: must be a number`);
     }
 
     return {
         bot: rule.bot,
-        task: rule.task || '',
+        task: rule.task,
         action: rule.action || 'edit',
         namespace: rule.namespace && rule.namespace.match(/\d+/g).map(num => parseInt(num)),
         duration: rule.duration,
@@ -92,11 +92,12 @@ export function parseRule(rule: RawRule): Rule {
             (rule.title_regex && new RegExp('^' + rule.title_regex + '$')),
         summaryRegex: (rule.summary && new RegExp('^' + mwn.util.escapeRegExp(rule.summary) + '$')) ||
             (rule.summary_regex && new RegExp('^' + rule.summary_regex + '$')),
-        alertMode: rule.alert_mode || 'talkpage',
-        alertPage: rule.alert_page || 'User talk:' + rule.bot,
-        emailUser: rule.email_user || rule.bot,
-        pingUser: rule.ping_user,
-        header: rule.header,
-        minEdits: rule.min_edits ? parseInt(rule.min_edits) : 1
+        minEdits: rule.min_edits ? parseInt(rule.min_edits) : 1,
+        alertPage
+
+        // alertMode: rule.alert_mode as 'talkpage' | 'email' | 'ping',
+        // emailUser: rule.email_user || rule.bot,
+        // pingUser: rule.ping_user,
+        // header: rule.header,
     };
 }
