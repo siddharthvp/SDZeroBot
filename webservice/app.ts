3a05b3395adeb9450520cd2b4291dc9a738efb8c
Siddharth VP <siddharthvp@gmail.com>
2021-07-26 22:39:02 +0530

    webservice: log date

--- webservice/app.ts
+++ webservice/app.ts
@@ -2,13 +2,14 @@ import * as createError from "http-errors";
 import * as express from "express";
 import * as path from "path";
 import * as cookieParser from "cookie-parser";
-import * as logger from "morgan";
+import * as morgan from "morgan";
 import * as hbs from 'hbs';
 import * as cors from 'cors';
 
 // All paths to SDZeroBot files must be via ../../SDZeroBot rather than via ../
 // The latter will work locally but not when inside toolforge www/js directory!
 import { bot, mwn } from "../../SDZeroBot/botbase";
+import { humanDate } from "../../mwn/build/log";
 import { ENWIKI_DB_HOST, TOOLS_DB_HOST } from "../../SDZeroBot/db";
 import { createLocalSSHTunnel } from "../../SDZeroBot/utils";
 import { registerRoutes } from "./route-registry";
@@ -22,7 +23,8 @@ hbs.registerHelper('wikilink',  (pageName, displayName) => {
 	return `<a href="https://en.wikipedia.org/wiki/${mwn.util.wikiUrlencode(pageName)}" title="${pageName.replace(/"/g, '&#34;')}">${typeof displayName === 'string' ? displayName : pageName}</a>`;
 });
 
-app.use(logger('dev'));
+morgan.token('date', () => humanDate());
+app.use(morgan('[:date] :method :url :status :response-time ms'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
