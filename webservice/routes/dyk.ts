3b4e7ab68eeb9481f75d78c48a3d122fa263aed4
Siddharth VP <siddharthvp@gmail.com>
2021-09-18 00:23:08 +0530

    add api endpoint to get number of dyk noms for user

--- webservice/routes/dyk.ts
+++ webservice/routes/dyk.ts
@@ -8,12 +8,28 @@ const db = new enwikidb();
 router.get('/credits/:user', async (req, res, next) => {
 	const user = req.params.user.replace(/ /g, '_');
 	const result = await db.query(`
-		SELECT COUNT(*) AS count from revision_userindex
+		SELECT COUNT(*) AS count FROM revision_userindex
 		JOIN page ON rev_page = page_id
 		JOIN actor_revision ON rev_actor = actor_id
 		WHERE page_namespace = 3
 		AND actor_name = 'DYKUpdateBot'
 		AND SUBSTRING_INDEX(page_title, '/', 1) = ?
+	`, [user]);
+	const count = result[0].count;
+	res.end(String(count));
+});
+
+router.get('/noms/:user', async (req, res, next) => {
+	const user = req.params.user.replace(/ /g, '_');
+	const result = await db.query(`
+		SELECT COUNT(*) AS count FROM revision_userindex
+		JOIN page ON rev_page = page_id
+		JOIN actor_revision ON rev_actor = actor_id
+		WHERE page_namespace = 10
+		AND page_is_redirect = 0
+		AND rev_parent_id = 0
+		AND page_title LIKE 'Did_you_know_nominations/%'
+		AND actor_name = ?
 	`, [user]);
 	const count = result[0].count;
 	res.end(String(count));
