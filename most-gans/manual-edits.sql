55713f39d986546b51749ac748c566abb8e8781b
Siddharth VP <siddharthvp@gmail.com>
2022-08-06 14:13:00 +0530

    most-gans: +1 in manual-edits.sql
    https://en.wikipedia.org/wiki/Wikipedia_talk:Good_article_nominations#Help!

--- most-gans/manual-edits.sql
+++ most-gans/manual-edits.sql
@@ -2,3 +2,4 @@ replace into nominators2 values('Algonquin Round Table', 'Otto4711', '2007-09-08
 replace into nominators2 values('Georgetown Hoyas', 'Mareino', '2006-04-05');
 replace into nominators2 values('Finn the Human / Jake the Dog', 'Gen. Quon', '2014-01-12');
 replace into nominators2 values('K-43 (Kansas highway)', 'TCN7JM', '2013-02-11');
+replace into nominators2 values('James L. Buie', 'Doug Coldwell', '2022-08-03');
