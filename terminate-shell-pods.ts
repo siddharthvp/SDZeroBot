d0119c7b2d28d9b5103417bb3873968a61c88edb
Siddharth VP <siddharthvp@gmail.com>
2022-11-16 09:02:03 +0530

    fix terminate-shell-pods.ts; setup cron job

--- terminate-shell-pods.ts
+++ terminate-shell-pods.ts
@@ -1,18 +1,19 @@
 import {bot, log} from "./botbase";
-import k8s from "@kubernetes/client-node";
+import {KubeConfig, CoreV1Api} from "@kubernetes/client-node";
 
 (async function () {
-    const kc = new k8s.KubeConfig();
+    const kc = new KubeConfig();
     kc.loadFromDefault();
-    const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+    const k8sApi = kc.makeApiClient(CoreV1Api);
+    const namespace = 'tool-sdzerobot';
 
-    const listRequest = await k8sApi.listNamespacedPod('tool-sdzerobot');
+    const listRequest = await k8sApi.listNamespacedPod(namespace);
     const pods = listRequest.body.items;
     for (const pod of pods) {
         const podName = pod.metadata.name;
         if (podName.startsWith('shell-') && isStale(pod.metadata.creationTimestamp)) {
             log(`[W] Terminating dangling pod ${podName}`);
-            await k8sApi.deleteNamespacedPod(podName, 'tool-sdzerobot');
+            await k8sApi.deleteNamespacedPod(podName, namespace);
         }
     }
 })();
