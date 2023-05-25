5ba542732acb819126983a47cb629d58d56a9b82
Siddharth VP <siddharthvp@gmail.com>
2023-05-25 21:33:21 +0530

    fix attribution for `Forge Park/495 station`
    https://en.wikipedia.org/w/index.php?title=User_talk:Novem_Linguae&oldid=1156975846#GAN_count

--- most-gans/manual-edits.sql
+++ most-gans/manual-edits.sql
@@ -3,3 +3,4 @@ REPLACE INTO nominators VALUES('Georgetown Hoyas', 'Mareino', '2006-04-05', '202
 REPLACE INTO nominators VALUES('Finn the Human and Jake the Dog', 'Gen. Quon', '2014-01-12', '2023-02-23');
 REPLACE INTO nominators VALUES('K-43 (Kansas highway)', 'TCN7JM', '2013-02-11', '2022-08-06');
 REPLACE INTO nominators VALUES('James L. Buie', 'Doug Coldwell', '2022-08-03', '2022-08-06');
+REPLACE INTO nominators VALUES('Forge Park/495 station', 'Pi.1415926535', '2022-12-24', '2023-05-25');
