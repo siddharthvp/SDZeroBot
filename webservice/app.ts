9efbe73910370a1881f2186b8cf9441051fb801b
Siddharth VP <siddharthvp@gmail.com>
2023-09-11 14:55:55 +0530

    add tank image and add a header for public assets

--- webservice/app.ts
+++ webservice/app.ts
@@ -30,7 +30,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+	setHeaders: (res, path, stat) => {
+		res.setHeader('Supports-Loading-Mode', 'fenced-frame')
+	}
+}));
 
 // bot account setup: get siteinfo once and refresh tokens every 10 minutes
 // XXX: more systematic way for token handling?
