f4458c2174ab3770e37f5d6b6db1f35c56a74dd8
Siddharth VP <siddharthvp@gmail.com>
2023-11-20 14:28:14 +0530

    g13-1week: exclude pages in [[Category:All_drafts_subject_to_special_procedures]]

--- reports/g13-1week.ts
+++ reports/g13-1week.ts
@@ -30,7 +30,7 @@ async function runForDate(date: MwnDate) {
 		AND page_is_redirect = 0
 		AND rev_timestamp < "${startTs}"
 		AND rev_timestamp > "${endTs}"
-	
+		AND page_id NOT IN (SELECT cl_from FROM categorylinks WHERE cl_to = 'All_drafts_subject_to_special_procedures')
 		UNION
 		
 		SELECT DISTINCT page_namespace, page_title, rev_timestamp
