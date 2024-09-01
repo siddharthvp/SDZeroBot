acf228bebd1955562fe53781c91b3c139882655d
Siddharth VP <siddharthvp@gmail.com>
2024-09-01 22:37:35 +0530

    db-tabulator: rename js preprocess to postprocess for clarity

--- webservice/route-registry.ts
+++ webservice/route-registry.ts
@@ -10,12 +10,14 @@ import dykRouter from '../dyk-counts/web-endpoint';
 import gitsync from "./routes/gitsync";
 import botMonitorRouter from '../bot-monitor/web-endpoint'
 import gitlabRouter from './routes/gitlab';
+import autoSqlRouter from "../db-tabulator/autosql/web-endpoint";
 
 export function registerRoutes(app: express.Router) {
 	app.use('/', indexRouter);
 	app.use('/logs', logsRouter);
 	app.use('/logs.php', logsRouter); // support old URLs from the time webservice was in php
 	app.use('/database-report', dbReportRouter);
+	app.use('/autosql', autoSqlRouter)
 	app.use('/gans', gansRouter);
 	app.use('/summary', summaryRouter);
 	app.use('/articlesearch', articleSearchRouter);
