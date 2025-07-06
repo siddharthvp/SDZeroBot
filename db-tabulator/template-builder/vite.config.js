c98f88151365478929687d5bbdd241b9f244608e
Siddharth VP <siddharthvp@gmail.com>
2025-07-06 18:46:55 +0530

    db-tabulator: setup build config for template-builder

--- db-tabulator/template-builder/vite.config.js
+++ db-tabulator/template-builder/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../../webservice/public/database-report/template-builder',
+    assetsDir: 'database-report/template-builder',
+    emptyOutDir: true,
+    // This doesn't fully work, requires a hack in the package.json build command
+  }
 })
