eca25f66cb86b8b7b73dca80892c28ecdd37ddb2
Siddharth VP <siddharthvp@gmail.com>
2026-07-07 09:44:32 +0530

    pending-afc: reduce size of excerpts to avoid contenttoobig error

--- reports/pending-afc.ts
+++ reports/pending-afc.ts
@@ -86,7 +86,7 @@ import {NS_DRAFT} from "../namespaces";
 				continue;
 			}
 			let text = rev.content;
-			let excerpt = TextExtractor.getExtract(text, 250, 500);
+			let excerpt = TextExtractor.getExtract(text, 150, 300);
 			if (excerpt === '') { // empty extract is suspicious
 				if (/^\s*#redirect/i.test(text)) { // check if it's a redirect
 					// the db query should omit redirects, this happens only because of db lag
